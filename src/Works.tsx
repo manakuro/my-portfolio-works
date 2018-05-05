9e16d96d5200dfa1272dbf772d3d681390a9d74f
manato kuroda <a7108100@gmail.com>
2018-05-05 11:16:37 +0900

    refactor: fix types

--- src/Works.tsx
+++ src/Works.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import sampleImg from './static/images/sample.jpeg';
 
 interface IWorksProps {
-  showOverlay: (e: any) => void;
+  showOverlay: (e: Element) => void;
 }
 
 export default class Works extends React.Component<IWorksProps, {}> {
