1c9a2382a31e141d492d9b36b087ba747387aae8
manato kuroda <a7108100@gmail.com>
2018-05-20 11:49:28 +0900

    refactor: remove semi

--- src/reducers/home/index.ts
+++ src/reducers/home/index.ts
@@ -1,23 +1,23 @@
-import types from '@/reducers/home/action-types';
-import sampleImg from '@/static/images/sample.jpeg';
+import types from '@/reducers/home/action-types'
+import sampleImg from '@/static/images/sample.jpeg'
 
 /* interfaces */
 export interface IWork {
   id: number
   title: string
-  description: string;
-  img: string;
-  component: string;
+  description: string
+  img: string
+  component: string
 }
 
 export interface IHomeState {
-  works: IWork[];
-  workContentImg: string;
-  isShowOverlay: boolean;
-  isShowWorksContent: boolean;
-  isShowWorksContentAnimation: boolean;
-  circleStyle: React.CSSProperties;
-  targetWork: IWork | null;
+  works: IWork[]
+  workContentImg: string
+  isShowOverlay: boolean
+  isShowWorksContent: boolean
+  isShowWorksContentAnimation: boolean
+  circleStyle: React.CSSProperties
+  targetWork: IWork | null
 }
 
 /* reducer */
@@ -39,35 +39,35 @@ const initialState: IHomeState = {
   isShowWorksContentAnimation: false,
   circleStyle: {},
   targetWork: null,
-};
+}
 
-export default function home(state: IHomeState = initialState, action: any ) {
+export default function home(state: IHomeState = initialState, action: any) {
   switch (action.type) {
     case types.TOGGLE_OVERLAY:
-      const { isShowOverlay } = action;
-      return { ...state, isShowOverlay };
+      const { isShowOverlay } = action
+      return { ...state, isShowOverlay }
 
     case types.TOGGLE_WORKS_CONTENT:
-      const { isShowWorksContent } = action;
-      return { ...state, isShowWorksContent };
+      const { isShowWorksContent } = action
+      return { ...state, isShowWorksContent }
 
     case types.TOGGLE_WORKS_CONTENT_ANIMATION:
-      const { isShowWorksContentAnimation } = action;
-      return { ...state, isShowWorksContentAnimation };
+      const { isShowWorksContentAnimation } = action
+      return { ...state, isShowWorksContentAnimation }
 
     case types.UPDATE_CIRCLE:
-      const { circleStyle } = action;
-      return { ...state, circleStyle };
+      const { circleStyle } = action
+      return { ...state, circleStyle }
 
     case types.UPDATE_WORK_CONTENT_IMG:
-      const { workContentImg } = action;
-      return { ...state, workContentImg };
+      const { workContentImg } = action
+      return { ...state, workContentImg }
 
     case types.UPDATE_TARGET_WORK:
-      const { payload } = action;
-      return { ...state, targetWork: payload };
+      const { payload } = action
+      return { ...state, targetWork: payload }
 
     default:
-      return state;
+      return state
   }
 }
